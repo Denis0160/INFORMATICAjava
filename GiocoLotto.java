d1ae20cb6e60f893e6032bdfb91189d525b4e929
Denis <150442493+Denis0160@users.noreply.github.com>
2024-02-02 13:22:12 +0100

    ultimo lotto

--- GiocoLotto.java
+++ GiocoLotto.java
@@ -2,24 +2,139 @@ import java.util.Scanner;
 import java.util.Random;
 public class GiocoLotto {
     public static void main(String[] Args) {
-        int scelta;
-        int sceltaruota1;
-        int N;
+        int scelta,dim=0;
+        double puntata1=0, puntata2=0, puntata3=0, puntata4=0, puntata5=0;
+        boolean s1=false,s2=false,s3=false,s4=false,s5=false;
+        int cont1=0,cont2,cont3,cont4,cont5;
+        int sceltapuntata=0;
+        int[] NumUtente=new int[5];
         Scanner tastiera = new Scanner(System.in);
 
-        String[] opzioni = {"=== Gioco Lotto === ",
-                "Su quante ruote vuoi giocare?",
-                "[1] Una ruota",
-                "[2] Dieci ruote",
-                "[3] Esci",};
+        System.out.println("=== Gioco Lotto === \nSu quante ruote vuoi giocare?\n");
+        String[] opzioni = {"[1] Una ruota",
+                            "[2] Dieci ruote",
+                            "[3] Esci",};
         scelta = Menu(opzioni, tastiera);
-            switch(scelta){
+
+        switch(scelta){
+            case 1:{
+                dim=5;
+                break;
+            }
+            case 2:{
+                dim=50;
+                break;
+            }
+        }
+        int[] ruotaN=new int[dim];
+        for(int i=0;i< ruotaN.length;i++) {
+            ruotaN[i] = numRand(1, 90);
+            System.out.println(ruotaN[i]);
+        }
+
+        System.out.println("Gioca i numeri");
+        for(int i=0;i<5;i++){
+            NumUtente[i]=tastiera.nextInt();
+            System.out.println(NumUtente[i]);
+        }
+        do{
+            System.out.println("----Scegli su cosa puntare----");
+            System.out.println("[1]singolo");
+            System.out.println("[2]Ambo");
+            System.out.println("[3]terna");
+            System.out.println("[4]quterna");
+            System.out.println("[5]cinquina");
+            System.out.println("[6]Continua");
+            sceltapuntata=tastiera.nextInt();
+            switch (sceltapuntata){
                 case 1:{
-                    System.out.println("quante ruote?");
-                    sceltaruota1 = tastiera.nextInt();
-                    Ruota1(sceltaruota1);
+                    if(s1==false){
+                        System.out.println("Hai scelto di puntare su singolo!");
+                        s1=true;
+                        do {
+                            System.out.println("Quanto vuoi puntare??");
+                            puntata1 = tastiera.nextDouble();
+                        }while(puntata1<0.50 || puntata1>200);
+                    }
+                    else if(s1==true)
+                    {
+                        System.out.println("Hai Gia scelto singolo");
+
+                    }
+                    break;
+                }
+                case 2:{
+                    if(s2==false){
+                        System.out.println("Hai scelto di puntare su ambo!");
+                        s2=true;
+                        do {
+                            System.out.println("Quanto vuoi puntare??");
+                            puntata2 = tastiera.nextDouble();
+                        }while(puntata2<0.50 || puntata2>200);
+                    }
+                    else if(s2==true)
+                    {
+                        System.out.println("Hai Gia scelto ambo");
+                    }
+                    break;
+                }
+                case 3:{
+                    if(!s3){
+                        System.out.println("Hai scelto di puntare su terna!");
+                        s3=true;
+                        do {
+                            System.out.println("Quanto vuoi puntare??");
+                            puntata3 = tastiera.nextDouble();
+                        }while(puntata3<0.50 || puntata3>200);
+                    }
+                    else if(s3)
+                    {
+                        System.out.println("Hai Gia scelto terna");
+                    }
+                    break;
+                }
+                case 4:{
+                    if(!s4){
+                        System.out.println("Hai scelto di puntare su quaterna!");
+                        s4=true;
+                        do {
+                            System.out.println("Quanto vuoi puntare??");
+                            puntata4 = tastiera.nextDouble();
+                        }while(puntata4<0.50 || puntata4>200);
+                    }
+                    else if(s4)
+                    {
+                        System.out.println("Hai Gia scelto quaterna");
+                    }
+                    break;
+                }
+                case 5:{
+                    if(!s5){
+                        System.out.println("Hai scelto di puntare su cinquina!");
+                        s5=true;
+                        do {
+                            System.out.println("Quanto vuoi puntare??");
+                            puntata5= tastiera.nextDouble();
+                        }while(puntata5<0.50 || puntata5>200);
+                    }
+                    else if(s5)
+                    {
+                        System.out.println("Hai Gia scelto cinquina");
+                    }
+                    break;
                 }
             }
+        }while(sceltapuntata!=6);
+
+        for (int i = 0; i < NumUtente.length; i++) {
+            for (int k = 0; k < ruotaN.length; k++) {
+                if (NumUtente[i] == ruotaN[k]) {
+                    cont1++;
+                }
+            }
+        }
+        System.out.println("------------------"+cont1);
+
     }
 
     private static int Menu(String opzioni[], Scanner tastiera) {
@@ -27,9 +142,7 @@ public class GiocoLotto {
 
         do {
 
-            System.out.println("------------------");
             System.out.println(opzioni[0]);
-            System.out.println("------------------");
             for (int i = 1; i < opzioni.length; i++) {
                 System.out.println(opzioni[i]);
             }
@@ -37,6 +150,10 @@ public class GiocoLotto {
 
             if ((scelta < 1) || (scelta > opzioni.length - 1)) {
                 System.out.println("Opzione Sbagliata");
+                Wait();
+                ClrScr();
+
+
 
             }
         }
@@ -44,78 +161,32 @@ public class GiocoLotto {
 
         return scelta;
     }
-    private static int numRand()  // metodo per estrarre il dado
-    {
+    //METODO RANDOM
+    private static int numRand(int minValue, int maxValue) {
 
         Random num = new Random();
-        return num.nextInt(91);
-
-    }
-
-    private static void Ruota1(int sceltaruota1) {
-        int numeroruote=0;
-        int[] numeriruote=new int[50];
-        int P=0;
-        switch(sceltaruota1){
-            case 1:{
-                numeroruote=5;
-                P=1;
-            }
-            case 2:{
-                numeroruote=50;
-                P=5;
-            }
-        }
-        for (int i = 0; i < P; i++) {
-            System.out.println("°ruota:\n");
-            for (int k = 0; k < numeroruote; k++) {
-                numeriruote[k] = numRand();
-                System.out.println("."+numeriruote[i]);
-            }
-        }
-
-
-
+        return num.nextInt(maxValue)+minValue;
 
     }
 
 
 
-
-
-    private static int NumeroDiNumeriDaGiocare() {
-        Scanner scanner = new Scanner(System.in);
-        int quantità;
-        int scelta;
-        int importo=0;
-
-        do {
-            System.out.print("Inserisci quanti numeri vuoi giocare (da 1 a 5): ");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Input non valido. Inserisci un numero da 1 a 5.");
-                scanner.next();
-            }
-            quantità = scanner.nextInt();
-        } while (quantità < 1 || quantità > 5);
-
-        switch(quantità){
-            case 1:{
-                System.out.println("Vuoi puntare su Ambo?\n" +
-                        "\n1)si" +
-                        "\n2)no");
-                scelta= scanner.nextInt();
-                if(scelta==1){
-                  System.out.println("\nQuanto vuoi puntare su Ambo?");
-                  importo=scanner.nextInt();
-                }else{
-                    break;
-                }
+        private static void ClrScr(){
+            try {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-
-        return quantità;
+    private static void Wait() {
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
 
 }
-
-
